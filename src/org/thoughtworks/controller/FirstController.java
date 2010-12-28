3b7e4ddb5d08b3c26dd53a870f00f2c91e178b6a
vinci <vinci.zhang@gmail.com>
2010-12-28 14:24:28 +0800

    in work

--- src/org/thoughtworks/controller/FirstController.java
+++ src/org/thoughtworks/controller/FirstController.java
@@ -1,5 +1,15 @@
 package org.thoughtworks.controller;
 
-@RequestMapping()
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/test/")
 public class FirstController {
+
+    @RequestMapping(value="a", method = RequestMethod.GET)
+    public String test() {
+        return "test";
+    }
 }
